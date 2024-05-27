84cb38b6a9be1556540e777a57ef5e80616b6ce1
Adryanne Kelly <adryannekelly123@gmail.com>
2024-05-27 11:47:32 -0300

    feat: step by step section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'sans': ['Inter', 'sans-serif'],
-			},
-			colors: {
-				'fontColor': '#555555',
-				'primary': '#129166',
-				'bg-secondary': '#84CC6D',
-			}
-		},
-
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      colors: {
+        fontColor: "#555555",
+        primary: "#129166",
+        secondary: "#84CC6D",
+      },
+    },
+  },
+  plugins: [],
+};
