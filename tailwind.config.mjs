1b60b8b20bc90858fc8934a41df89e023680b437
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-05-28 19:45:09 -0300

    refactor: v2 refactor

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
+        staatliches : ["Staatliches", "sans"],
       },
       colors: {
         fontColor: "#555555",
